e503ab557785da3a452a51278d3ca378a6cdee5a
bsc <bsc@4xs.org>
2021-11-27 12:50:02 +0100

    Optimized code after working with Sonarqube according to Clean Code Development standards

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -4,69 +4,56 @@
 from random import randint
 
 # Initializing variables
-humanPoints = 0
-machinePoints = 0
-gameRound = 0
-maxGameRounds = 3
+human_points = 0
+machine_points = 0
+game_round = 0
+max_game_rounds = 3
 
-gameElement = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+ROCK = 0
+PAPER = 1
+SCISSORS = 2
+LIZARD = 3
+SPOCK = 4
 
-def returnWinnerOfRound(human, machine):
+# Defines the 5 game elements with a human-readable string
+GAME_ELEMENT = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+# This list contains the winning combinations and their corresponding actions
+WINNING_ELEMENT = {
+                    ROCK: { SCISSORS: "crushes", LIZARD: "crushes" },
+                    PAPER: { ROCK: "covers", SPOCK: "disproves" },
+                    SCISSORS: { PAPER: "cuts", LIZARD: "decapitates" },
+                    LIZARD: { PAPER: "eats", SPOCK: "poisons" },
+                    SPOCK: { ROCK: "vaporizes", SCISSORS: "smashes" }
+                }
+
+
+
+def return_winner_of_round(human, machine):
     """This function returns the winner of each round"""
     # This implements Sheldon Cooper's Rock, Paper, Scissors, Lizard, Spock game
     # from the TV Show "The Big Bang Theory"
-    #
-    # As Sheldon explains, 
-    # "Scissors cuts paper, 
-    # paper covers rock, 
-    # rock crushes lizard, 
-    # lizard poisons Spock, 
-    # Spock smashes scissors, 
-    # scissors decapitates lizard, 
-    # lizard eats paper, 
-    # paper disproves Spock, 
-    # Spock vaporizes rock, 
-    # and as it always has, rock crushes scissors."
     
-    global humanPoints, machinePoints
-    if human == machine:
-        message = "Draw game!"
-    elif human == 2 and machine == 1:
-        humanPoints += 1
-        message = "Scissors cuts Paper."
-    elif human == 1 and machine == 0:
-        humanPoints += 1
-        message = "Paper covers rock."
-    elif human == 0 and machine == 3:
-        humanPoints += 1
-        message = "Rock crushes Lizard."
-    elif human == 3 and machine == 4:
-        humanPoints += 1
-        message = "Lizard poisons Spock."
-    elif human == 4 and machine == 2:
-        humanPoints += 1
-        message = "Spock smashes Scissors."
-    elif human == 2 and machine == 3:
-        humanPoints += 1
-        message = "Scissors decapitates Lizard."
-    elif human == 3 and machine == 1:
-        humanPoints += 1
-        message = "Lizard eats Paper."
-    elif human == 1 and machine == 4:
-        humanPoints += 1
-        message = "Paper disproves Spock."
-    elif human == 4 and machine == 0:
-        humanPoints += 1
-        message = "Spock vaporizes Rock."
-    elif human == 0 and machine == 2:
-        humanPoints += 1
-        message = "Rock crushes Scissors."
-    else:
-        machinePoints += 1
-        message = "You lose!"
-    return message
+    global human_points, machine_points
 
-def returnWinnerOfGame(human, machine):
+    # If both the human and the machine chose the same weapon, it's a draw game.
+    if GAME_ELEMENT[human] == GAME_ELEMENT[machine]:
+        return "Draw game!"
+    # If not, check who has won
+    else:
+        # Iterate through the list of the respective winning element.
+        # If the machine chose an element that is in this list, it's a winning combination for the human player.
+        for weapon in WINNING_ELEMENT[human]:
+            if weapon == machine:
+                print(GAME_ELEMENT[human] + " " + WINNING_ELEMENT[human][weapon] + " " + GAME_ELEMENT[machine])
+                human_points += 1
+                return "You won!"
+
+        # If the machine chose an element that is *not* in this list, it's a losing combination for the human player.
+        machine_points += 1
+        return "You lose!"
+
+
+def return_winner_of_game(human, machine):
     """This function returns the winner of the game"""
     if human > machine:
         return "You won the game!"
@@ -75,28 +62,28 @@ def returnWinnerOfGame(human, machine):
     else:
         return "You've lost the game!"
 
-def runGame():
+def run_game():
     """This function runs the game"""
-    for i in range(maxGameRounds):    
+    for i in range(max_game_rounds):    
         print("Round", i + 1,"! Fight!")
         print("Your choice:")
         print("Remember: Rock = 0, Paper = 1, Scissors = 2, Lizard = 3, Spock = 4")
-        humanDraw = 5
+        human_draw = 5
 
         # Error handling in case the user enters an invalid number
-        while humanDraw > 4:
-            humanDraw = int(input())
-            if humanDraw <= 4:
+        while human_draw > 4:
+            human_draw = int(input())
+            if human_draw <= 4:
                 break
             else:
                 print("Please make sure to enter a number less than 5!")
 
-        print("Your draw:", gameElement[humanDraw])
+        print("Your draw:", GAME_ELEMENT[human_draw])
         
         # Random draw
-        machineDraw = randint(0,4)
-        print("Machine's draw:", gameElement[machineDraw])
-        print(returnWinnerOfRound(humanDraw, machineDraw),"\n")
-    print(returnWinnerOfGame(humanPoints, machinePoints))
+        machine_draw = randint(0,4)
+        print("Machine's draw:", GAME_ELEMENT[machine_draw])
+        print(return_winner_of_round(human_draw, machine_draw),"\n")
+    print(return_winner_of_game(human_points, machine_points))
 
-runGame()
+run_game()
